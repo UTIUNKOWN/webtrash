942f4d832ed22ce8c53d57889dc550fd3682b970
SitiiRokoiye <siti_r@student.untan.ac.id>
2023-07-27 20:32:43 +0700

    test

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -9,8 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
-    <link href="/css/dashboard.css" rel="stylesheet">
-    <link href="/css/features.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
     <link href="/img/trash-bin.png" rel="shortcut icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -48,48 +46,50 @@
 <body>
 
     <section class="home-section">
-        <div class="container" style="text-align: center; margin-top: 80 px">
 
-            <img src="{{ 'img/TRASH.jpg' }}" style="width: 400px"class="container"
-                style="text-align: center; margin-top: 10 px">
-            <h4 class="pt-3" style="font-size: 28px; font-weight: bold; color: #00c3e5;">MONITORING TEMPAT SAMPAH</h4>
-            {{-- <h5 class="mb-2">REKAYASA SISTEM KOMPUTER</h5> --}}
-            <hr>
-        </div>
-
-        {{-- kapasitas data 1 --}}
-        <div class="col-span-12 grid grid-cols-12 gap-6 mt-8">
-            <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
-                <div class="box p-5 zoom-in"style="background-color: #1072b8;">
-                    <div class="flex items-center">
-                        <div class="w-2/4 flex-none">
-                            <div class="text-lg font-medium truncate"style="color: white;">KAPASITAS</div>
-                            <div class="text-slate-500 mt-1"style="color: white;">TEMPAT SAMPAH 1</div>
-                        </div>
-                        <div class="flex-none ml-auto relative">
-                            <div id="progress" data-donut={{ $kapasitas1 }}></div>
-                        </div>
-
-                    </div>
-                </div>
+            <div class="container" style="text-align: center; margin-top: 80 px">
+                <img src="{{ 'img/TRASH.jpg' }}" style="width: 400px"class="container"
+                    style="text-align: center; margin-top: 10 px">
+                <h4 class="pt-3" style="font-size: 28px; font-weight: bold; color: #00c3e5;">MONITORING TEMPAT SAMPAH
+                </h4>
+                {{-- <h5 class="mb-2">REKAYASA SISTEM KOMPUTER</h5> --}}
+                <hr>
             </div>
-            {{-- kapasitas data 2 --}}
 
-            <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
-                <div class="box p-5 zoom-in"style="background-color: #1072b8;">
-                    <div class="flex items-center">
-                        <div class="w-2/4 flex-none">
-                            <div class="text-lg font-medium truncate"style="color: white;">KAPASITAS</div>
-                            <div class="text-slate-500 mt-1"style="color: white;">TEMPAT SAMPAH 2</div>
-                        </div>
-                        <div class="flex-none ml-auto relative">
-                            <div id="progress2" data-donut={{ $kapasitas2 }}></div>
+            {{-- kapasitas data 1 --}}
+            <div class="col-span-12 grid grid-cols-12 gap-6 mt-8">
+                <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+                    <div class="box p-5 zoom-in"style="background-color: #1072b8;">
+                        <div class="flex items-center">
+                            <div class="w-2/4 flex-none">
+                                <div class="text-lg font-medium truncate"style="color: white;">KAPASITAS</div>
+                                <div class="text-slate-500 mt-1"style="color: white;">TEMPAT SAMPAH 1</div>
+                            </div>
+                            <div class="flex-none ml-auto relative">
+                                <div id="progress" data-donut={{ $kapasitas1 }}></div>
+                            </div>
 
                         </div>
                     </div>
                 </div>
+                {{-- kapasitas data 2 --}}
+
+                <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+                    <div class="box p-5 zoom-in"style="background-color: #1072b8;">
+                        <div class="flex items-center">
+                            <div class="w-2/4 flex-none">
+                                <div class="text-lg font-medium truncate"style="color: white;">KAPASITAS</div>
+                                <div class="text-slate-500 mt-1"style="color: white;">TEMPAT SAMPAH 2</div>
+                            </div>
+                            <div class="flex-none ml-auto relative">
+                                <div id="progress2" data-donut={{ $kapasitas2 }}></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
 
         {{-- kapasitas  --}}
         {{-- <div class="col-span-12 grid grid-cols-12 gap-6 mt-8">
@@ -112,18 +112,14 @@
             </div>
         </div>
         <div class="row justify-content-center"> --}}
-            <div class="pt-2 text-center">
-                <div class="d-flex row justify-content-center">
-                    <a href="" class="text-decoration-none col-lg-3 col-md-4 col-sm-6 mb-3"
-                        data-bs-toggle="modal" data-bs-target="#exampleModal-1">
+        <div class="pt-2 text-center">
+            <div class="d-flex row justify-content-center">
+                <a href="" class="text-decoration-none col-lg-3 col-md-4 col-sm-6 mb-3" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal-1">
 
 
-                </div>
+            </div>
     </section>
-    </div>
-    </main>
-    </div>
-    </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
