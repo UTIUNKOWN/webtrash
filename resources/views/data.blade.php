cf0ca0c7a71ed3b63f0ba34ceebccc88f46b57d2
SitiiRokoiye <siti_r@student.untan.ac.id>
2023-09-15 19:51:25 +0700

    revisi

---
+++ resources/views/data.blade.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sudah Semhas Ni Boossssss</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+        rel="stylesheet">
+
+    <link href="{{ asset('template') }}/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="{{ asset('template') }}/css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="{{ asset('template/css/templatemo-tiya-golf-club.css') }}" rel="stylesheet">
+
+    <!--
+
+TemplateMo 587 Tiya Golf Club
+
+https://templatemo.com/tm-587-tiya-golf-club
+
+-->
+
+    <style>
+        /* Mengatur ukuran dan penempatan tabel */
+        .table-custom {
+            max-width: 70%;
+            /* Sesuaikan lebar maksimal yang diinginkan */
+            margin: auto;
+        }
+
+        /* Mengatur ukuran kolom */
+        .table-custom th,
+        .table-custom td {
+            text-align: center;
+            /* Penempatan teks di tengah kolom */
+            padding: 0.5rem;
+            /* Padding untuk sel */
+            font-size: 20px;
+            /* Ukuran font */
+        }
+
+        table {
+            margin-top: 150px;
+            /* Sesuaikan dengan tinggi navbar Anda */
+        }
+
+        table tbody tr {
+            height: 10px;
+            /* Sesuaikan tinggi yang diinginkan */
+        }
+
+
+        /* Mengaktifkan kembali scroll hanya untuk tabel */
+        section table {
+            overflow-y: auto;
+            max-height: 400px;
+            /* Sesuaikan dengan tinggi yang Anda inginkan */
+        }
+    </style>
+    {{-- <style>
+        /* Existing styles */
+
+        /* Add custom styles for pagination */
+        .pagination {
+            margin: 20px 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            list-style: none;
+        }
+
+        .pagination li {
+            margin: 0 4px;
+        }
+
+        .pagination a {
+            color: #007BFF;
+            padding: 8px 16px;
+            text-decoration: none;
+            border: 1px solid #007BFF;
+            border-radius: 5px;
+        }
+
+        .pagination a:hover {
+            background-color: #007BFF;
+            color: #fff;
+        }
+
+        .pagination .active a {
+            background-color: #007BFF;
+            color: #fff;
+        }
+
+        .pagination .disabled a {
+            pointer-events: none;
+            opacity: 0.5;
+        }
+    </style> --}}
+</head>
+
+<body>
+
+    <main>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand d-flex align-items-center" href="index.html">
+                    <img src="images/logo.png" class="navbar-brand-image img-fluid" alt="Tiya Golf Club">
+                    <span class="navbar-brand-text">
+                        Siti
+                    </span>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll " href="realtime">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_2"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll active text-warning" href="data">Data</a>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section>
+            <div class="container" style="margin-top: 150px; ">
+                <h2 class="text-center">Tabel Data bla bla</h2>
+                <table class="table table-success table-striped table-custom">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px;">Waktu</th>
+                            <th style="width: 50px;">Tempat Sampah</th>
+                            <th style="width: 50px;">Kapasitas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($datas as $dat)
+                            <tr>
+                                <td>{!! $dat->created_at !!}</td>
+                                <td>{{ $dat->id_sensor }}</td>
+                                <td>{!! $dat->kapasitas !!}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="3" class="text-center">
+                                    <div class="alert alert-danger">
+                                        Data belum Tersedia.
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+
+            </div>
+            @if($datas->hasPages())
+            <div class="card-footer">
+                {{ $datas->links() }}
+            </div>
+            @endif
+        </section>
+
+    </main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12 me-auto mb-5 mb-lg-0">
+                    <a class="navbar-brand d-flex align-items-center" href="index.html">
+                        <img src="images/logo.png" class="navbar-brand-image img-fluid" alt="">
+                        <span class="navbar-brand-text">
+                            Sudah Semhas Ni Boss
+                        </span>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-12">
+                    <h5 class="site-footer-title mb-4">Join Us</h5>
+
+                    <p class="d-flex border-bottom pb-3 mb-3 me-lg-3">
+                        <span>Mon-Fri</span>
+                        6:00 AM - 6:00 PM
+                    </p>
+
+                    <p class="d-flex me-lg-3">
+                        <span>Sat-Sun</span>
+                        6:30 AM - 8:30 PM
+                    </p>
+                    <br>
+                    <p class="copyright-text">Copyright © 2048 Tiya Golf Club</p>
+                </div>
+
+                <div class="col-lg-2 col-12 ms-auto">
+                    <ul class="social-icon mt-lg-5 mt-3 mb-4">
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-instagram"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-twitter"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-whatsapp"></a>
+                        </li>
+                    </ul>
+                    <p class="copyright-text">Design: <a rel="nofollow" href="https://templatemo.com"
+                            target="_blank">TemplateMo</a></p>
+
+                </div>
+
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#81B29A" fill-opacity="1"
+                d="M0,224L34.3,192C68.6,160,137,96,206,90.7C274.3,85,343,139,411,144C480,149,549,107,617,122.7C685.7,139,754,213,823,240C891.4,267,960,245,1029,224C1097.1,203,1166,181,1234,160C1302.9,139,1371,117,1406,106.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+            </path>
+        </svg>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="{{ asset('template') }}/js/jquery.min.js"></script>
+    <script src="{{ asset('template') }}/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('template') }}/js/jquery.sticky.js"></script>
+    <script src="{{ asset('template') }}/js/click-scroll.js"></script>
+    <script src="{{ asset('template') }}/js/animated-headline.js"></script>
+    <script src="{{ asset('template') }}/js/modernizr.js"></script>
+    <script src="{{ asset('template') }}/js/custom.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            setInterval(function() {
+                $("#progress").load("{{ url('kapasitassampah') }}");
+                $("#progress2").load("{{ url('kapasitassampah2') }}");
+
+            }, 1000);
+        });
+    </script>
+
+
+
+</body>
+
+</html>
