1c1ee62cd8c72d4ff9c3a57e371742517337234e
SitiiRokoiye <siti_r@student.untan.ac.id>
2023-09-15 20:11:20 +0700

    revisi

--- resources/views/realtime.blade.php
+++ resources/views/realtime.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Sudah Semhas Ni Boossssss</title>
+        <title>pantau sampah</title>
 
         <!-- CSS FILES -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -38,15 +38,15 @@ https://templatemo.com/tm-587-tiya-golf-club
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
                     <a class="navbar-brand d-flex align-items-center" href="index.html">
-                        <img src="images/logo.png" class="navbar-brand-image img-fluid" alt="Tiya Golf Club">
+                        <img src="images/logo.png" class="navbar-brand-image img-fluid" alt="PENELITIAN SITI">
                         <span class="navbar-brand-text">
                             Siti
                         </span>
                     </a>
 
-                    <div class="d-lg-none ms-auto me-3">
+                    {{-- <div class="d-lg-none ms-auto me-3">
                         <a class="btn custom-btn custom-border-btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">Member Login</a>
-                    </div>
+                    </div> --}}
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -72,7 +72,7 @@ https://templatemo.com/tm-587-tiya-golf-club
                 </div>
             </nav>
 
-            <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            {{-- <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="offcanvasExampleLabel">Member Login</h5>
 
@@ -82,7 +82,7 @@ https://templatemo.com/tm-587-tiya-golf-club
 
 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3D405B" fill-opacity="1" d="M0,224L34.3,192C68.6,160,137,96,206,90.7C274.3,85,343,139,411,144C480,149,549,107,617,122.7C685.7,139,754,213,823,240C891.4,267,960,245,1029,224C1097.1,203,1166,181,1234,160C1302.9,139,1371,117,1406,106.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
-            </div>
+            </div> --}}
 
 
             <section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
@@ -106,12 +106,15 @@ https://templatemo.com/tm-587-tiya-golf-club
                             </h1>
 
                             <div class="custom-btn-group">
-                                <a href="#section_2" class="btn custom-btn smoothscroll me-3">VIEW DATA REALTIME</a>
+                                <a href="#section_2" class="btn custom-btn smoothscroll me-10">VIEW DATA REALTIME</a>
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-12 d-flex justify-content-end">
+                        {{-- <div class="col-lg-6 col-12 d-flex justify-content-end">
                             <img src="/img/Icon-Sampah.png" alt="" srcset="">
+                        </div> --}}
+                        <div class="col-lg-6 col-12 d-flex justify-content-end">
+                            <img src="/img/Icon-Sampah.png" class="img-fluid" alt="Responsive image" style="max-width: 200; height: auto;">
                         </div>
 
                     </div>
